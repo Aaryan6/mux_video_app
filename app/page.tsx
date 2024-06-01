ba8a33c994741cc933777c4fdcadbbc6647c31fd
Aaryan6 <aaryanpatel683@gmail.com>
2024-06-01 16:52:38 +0530

    Refactor video player and image loading on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import MuxPlayer from "@mux/mux-player-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="py-20 px-4">
       <div className="bg-gray-700 p-4 grid">
         MUX
         <MuxPlayer
           playbackId="gT400QaLbyGkoUTmkxV5zWO5eKokbehry3jDDdgkumeM"
-          className="w-[40rem] h-[20rem]"
+          className="w-full h-auto flex-1"
         />
       </div>
       <div className="bg-gray-700 p-4 mt-3">
@@ -16,9 +16,7 @@ export default function Home() {
         <video
           src="https://oukdqujzonxvqhiefdsv.supabase.co/storage/v1/object/public/course-videos/Generative AI Crash Course - Cohort IV (Week 1)/intro-1716734810389"
           controls
-          width={500}
-          height={500}
-          className="w-[40rem] h-[20rem]"
+          className="w-full h-auto"
         />
       </div>
     </main>
